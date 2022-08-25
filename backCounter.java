b67b136a5ecb509e2cec0858d331ff31d1a9a43c
Leonardo Montes <lmontes@HXMAC-2043.local>
2022-08-24 21:09:48 -0500

    First commit

---
+++ backCounter.java
@@ -0,0 +1,16 @@
+import java.util.Iterator;
+
+public class backCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int counter = 99;
+		
+		for (int i = counter; i >= 0 ; i--) {
+			System.out.println("Counter: " + counter --);
+		}
+		
+		
+	}
+
+}
